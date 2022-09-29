0413d700ca9794ed603ee1074fc59b5ef7ef4c46
matialba22 <albarracinmatias9@gmail.com>
2022-09-29 19:59:49 -0300

    subo todo al repo

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(3000, () => console.log('El servidor está corriendo en el puerto 3000'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
+
+app.get('/ingreso', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})

ba4e0b67d7a2b803c74b1e0e32db0b328988f592
matialba22 <albarracinmatias9@gmail.com>
2022-10-06 06:10:33 -0300

    agregue el puerto para heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, './public')));
 
-app.listen(3000, () => console.log('El servidor está corriendo en el puerto 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('El servidor está corriendo en el puerto 3000'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
